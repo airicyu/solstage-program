d0f216748a0b9125c832c7f011b1725ab1fdcebc
airicyu <airic.yu@gmail.com>
2024-03-25 00:50:27 +0800

    update program

--- programs/solstage-program/src/lib.rs
+++ programs/solstage-program/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("GWuqc8hjNbD4wRF37NAsdtF7kzKmMLbL17DJh65t8SsS");
+declare_id!("STAGYb3HZJW8CV1QEbhyc8WkLuJt44RtWHDpRgHe2pX");
 
 #[program]
 pub mod solstage_program {
@@ -9,12 +9,12 @@ pub mod solstage_program {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Initializing Filter Source");
         let filter_source = &mut ctx.accounts.filter_source;
-        filter_source.signature = [0; 64];
+        filter_source.signature = [0; 32];
         filter_source.url = "".to_string();
         Ok(())
     }
 
-    pub fn SetFilter(ctx: Context<SetFilter>, signature: [u8; 64], url: String) -> Result<()> {
+    pub fn set_filter(ctx: Context<SetFilter>, signature: [u8; 32], url: String) -> Result<()> {
         msg!("Setting Filter");
         let filter_source = &mut ctx.accounts.filter_source;
         filter_source.signature = signature;
@@ -28,7 +28,7 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 
-    #[account(init, payer = signer, space = 8 + 64 + 4 + 255,
+    #[account(init, payer = signer, space = 8 + 32 + 4 + 255,
         seeds = [b"filterSource:".as_ref(), signer.key.as_ref(), b":default".as_ref()], bump
     )]
     pub filter_source: Account<'info, FilterSource>,
@@ -52,6 +52,6 @@ pub struct SetFilter<'info> {
 
 #[account]
 pub struct FilterSource {
-    signature: [u8;64],
+    signature: [u8;32],
     url: String
 }
--- tests/solstage-program.ts
+++ tests/solstage-program.ts
@@ -1,16 +1,55 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolstageProgram } from "../target/types/solstage_program";
+import { PublicKey } from "@solana/web3.js";
+import {sign} from 'crypto'
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import nacl from "tweetnacl";
 
 describe("solstage-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolstageProgram as Program<SolstageProgram>;
 
-  it("Is initialized!", async () => {
+  it("Is initialized", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [filterSourcePDA, _] = PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("filterSource:"),
+      provider.wallet.publicKey.toBuffer(),
+      anchor.utils.bytes.utf8.encode(":default"),
+    ],
+    program.programId
+    );
+
+    const tx = await program.methods.initialize().accounts({
+      filterSource:filterSourcePDA
+    }).rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Is set filter source", async () => {
+    // Add your test here.
+    const [filterSourcePDA, _] = await PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("filterSource:"),
+      provider.wallet.publicKey.toBuffer(),
+      anchor.utils.bytes.utf8.encode(":default"),
+    ],
+    program.programId
+    );
+
+    const sourceSignature = Array.from(nacl.sign.detached(Buffer.from("{}"),(provider.wallet as NodeWallet).payer.secretKey));
+    // const sourceSignature = Array.from(sign("X25519", Buffer.from("{}"), Buffer.from((provider.wallet as NodeWallet).payer.secretKey.slice(0,32))))
+
+    const tx = await program.methods.setFilter(
+       sourceSignature ,
+       "http://localhost:3000/",
+    ).accounts({
+      filterSource:filterSourcePDA
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
